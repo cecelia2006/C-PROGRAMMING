899967990c9411557ef457b9b2f1369bb4b071f7
cecelia2006 <ceceliajimkwemoi@gmail.com>
2025-09-23 17:00:56 +0300

    Add files via upload

---
+++ incomeloan.c
@@ -0,0 +1,24 @@
+// Name:Obunga Cecelia Auma
+// Reg No:PA106/G/28829/25
+//Description:program to calculate Simple Interest
+#include <stdio.h>
+int main(){
+float income;
+int age;
+printf("enter your age:");
+scanf("%d",&age);
+printf("enter your annual income");
+scanf("%f",&income);;
+
+if(age>=21& income>=21000)
+{printf("congratulations,you qualify for a loan.\n");
+}
+else
+{
+printf("Unfortunatel we are unable to offer a loan at this time.\n");
+}
+
+    return 0;
+
+
+}
